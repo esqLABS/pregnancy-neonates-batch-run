6d8608810b5f250e34f71c206ef2a61e9eaaf8b6
Susana Proença <susana.proenca@esqlabs.com>
2025-01-14 15:11:10 +0100

    changed the path test file with chemicals

--- test_files/Run_different_models.R
+++ test_files/Run_different_models.R
@@ -1,5 +1,6 @@
 #Test function#--------
 #It runs all the partitions just to check if there are no bugs
+#It does take 10 min to run
 
 #get the function
 source("Neonate and Pregnancy sim function.R")
@@ -12,19 +13,20 @@ dataModel<-data.frame(matrix(ncol = 4, nrow = nChemicals))
 mergeDataModel<-data.frame()
 
 for (i in seq(1:length(age))){
-    ### make this a dataframe
+  ### make this a dataframe
   runSimulation[[i]]<-Run_batch(individual=age[i],partitionQSPR=PKalgor[i],
-                                    Dose_mg_kg=1,highResol=0.33,lowResol=0.07)
+                                    Dose_mg_kg=1,highResol=0.33,lowResol=0.07,
+                                permeability="simple")
   if (age[i]=="6_months"|age[i]=="2_weeks"){
-  dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tableCmax),
-                        "Cmax_plasma"=runSimulation[[i]]$tableCmax[,1],
+  dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tb_results),
+                        "Cmax_plasma"=runSimulation[[i]]$tb_results[,1],
                         "age"=rep(age[i],nrow(input_physchem)),
                         "PartitionQSPR"=rep(PKalgor[i],nrow(input_physchem)))
   
   } else if (age[i]=="GW15"|age[i]=="GW24"){
     
-    dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tableCmax),
-                          "Cmax_plasma"=runSimulation[[i]]$tableCmax[,3],
+    dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tb_results),
+                          "Cmax_plasma"=runSimulation[[i]]$tb_results[,3],
                           "age"=rep(age[i],nrow(input_physchem)),
                           "PartitionQSPR"=rep(PKalgor[i],nrow(input_physchem)))
     
