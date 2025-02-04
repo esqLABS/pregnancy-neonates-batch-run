70d673eec47fe3224faf512546970413feaffda5
Susana Proença <susana.proenca@esqlabs.com>
2025-02-04 19:48:23 +0100

    reorganization

--- test_files/Run_different_models.R
+++ test_files/Run_different_models.R
@@ -15,14 +15,14 @@ mergeDataModel<-data.frame()
 for (i in seq(1:length(age))){
   ### make this a dataframe
   runSimulation[[i]]<-Run_batch(individual=age[i],partitionQSPR=PKalgor[i],
-                                    Dose_mg_kg=1,highResol=0.33,lowResol=0.07,
-                                permeability="simple")
+                                Dose_mg_kg=1,highResol=0.33,lowResol=0.07,
+                                permeability="Normal")
   if (age[i]=="6_months"|age[i]=="2_weeks"){
-  dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tb_results),
-                        "Cmax_plasma"=runSimulation[[i]]$tb_results[,1],
-                        "age"=rep(age[i],nrow(input_physchem)),
-                        "PartitionQSPR"=rep(PKalgor[i],nrow(input_physchem)))
-  
+    dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tb_results),
+                          "Cmax_plasma"=runSimulation[[i]]$tb_results[,1],
+                          "age"=rep(age[i],nrow(input_physchem)),
+                          "PartitionQSPR"=rep(PKalgor[i],nrow(input_physchem)))
+    
   } else if (age[i]=="GW15"|age[i]=="GW24"){
     
     dataModel<-data.frame("Chemicals"=rownames(runSimulation[[i]]$tb_results),
@@ -32,7 +32,7 @@ for (i in seq(1:length(age))){
     
   }else{}
   mergeDataModel<-bind_rows(mergeDataModel,dataModel)
-
+  
 }
 colnames(mergeDataModel)=c("Chemicals","Cmax_plasma","age","PartitionQSPR")
 
