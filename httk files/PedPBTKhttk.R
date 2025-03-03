e23941673100a703da354d596d6512e720621106
Susana Proença <susana.proenca@esqlabs.com>
2025-03-03 15:59:08 +0100

    updated from Anna

--- httk files/PedPBTKhttk.R
+++ httk files/PedPBTKhttk.R
@@ -3,8 +3,8 @@ rm(list=ls(all=TRUE))
 library(httk)
 date.string <- Sys.Date()       
 
-physiology.data
-write.table(physiology.data, file = "httk_physio_data.txt", sep='\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+#physiology.data
+#write.table(physiology.data, file = "httk_physio_data.txt", sep='\t', col.names = TRUE, row.names = FALSE, quote = FALSE) #why do i read this in?
 
 species_1 <- "Human" 
 #expDose <- 1         
@@ -29,12 +29,17 @@ if(route != "iv"){
 
 for(this.cas in chemical[,"CASRN"]) {
   this.chem <-  chemical$Substance.Name[chemical$CASRN==this.cas]
-  parm2w <- parameterize_pbtk_adipose_brain(chem.cas= this.cas)
-  parm2w$BW<-8.29 #3.7 for 2w & 8.29 for 6m as from GastroPlus
-  output <- solve_pbtk_adipose_brain(chem.cas = this.cas, parameters = parm2w, tsteps = 21, doses.per.day = dpd, days = 1, 
+  parm <- parameterize_pbtk_adipose_complexbrain(chem.cas= this.cas)
+  parm$BW<-8.29 #3.7 for 2w & 8.29 for 6m as from GastroPlus
+  output <- solve_pbtk_adipose_complexbrain(chem.cas = this.cas, parameters = parm, tsteps = 21, doses.per.day = dpd, days = 1, 
                              dose = 1, iv.dose =FALSE, output.units = outputUnit, 
                              default.to.human = TRUE, plots = FALSE, suppress.messages = TRUE)
 
-  write.table(output, file = paste(getwd(), '/brain/6m_tstep_', this.chem, '.csv', sep=''), sep=',', col.names = TRUE, row.names = FALSE, quote = FALSE)
-
+  #write.table(output, file = paste(getwd(), '/output2w', this.chem, '.csv', sep=''), sep=',', col.names = TRUE, row.names = FALSE, quote = FALSE)
+#  write.table(output, file = paste(getwd(), 'PedPhI', date.string, this.chem, '.csv', sep=''), sep=',', col.names = TRUE, row.names = FALSE, quote = FALSE)
+ 
+param <- parameterize_pbtk_adipose_complexbrain(chem.cas = this.cas, suppress.messages = TRUE)  
+  
+write.table(param, file = paste(getwd(), '/parm6m_', this.chem, '.csv', sep=''), sep=',', col.names = TRUE, row.names = FALSE, quote = FALSE)
+  
 }
